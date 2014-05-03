76b9228e481ede6b675d45efc5c45e97c596cc65
jan <info@jcroonen.nl>
2014-05-03 19:23:10 +0200

    no message

--- scripts/openAH.js
+++ scripts/openAH.js
@@ -1,12 +1,14 @@
 (function(){
   /*
    * Author: Jan Croonen 2013.
+   *
+   * Open AvaxHome links for downloads.
    */
 
   function isDownloadLink(f, svr) {
       return f.indexOf(svr) !== -1;
   }
-  
+
   function dl(href, handle) {
     window.open(href, handle, 'width=400,height=400');
   }
@@ -18,19 +20,14 @@
       fp_srv = 'http://filepost.com/files';
 
   console.log(linksLen);
-  
+
   for (i = 0; i < linksLen; i++) {
     link = links[i];
     href = link.href;
     if (isDownloadLink(href, 'filepost.com')) {
-        //console.log(href);
-        //window.open(href, 'fp', 'width=40,height=40');
         setTimeout(function() {
             dl(href, 'fp');
         }, 500);
-        // testing . . .
-        //if (i==2) break;
-        //break;
     }
   }
 }());
