d58f3472b446dce7826dcbf3830a56e6f36e57bb
jan <info@jcroonen.nl>
2014-06-05 16:36:16 +0200

    haal print css op (maar niet gebruikt)

--- scripts/readtext.js
+++ scripts/readtext.js
@@ -2,10 +2,35 @@
  * Author: jan
  * Date: 15-mei-2014
  */
-var  href = document.location.href,
-     link = encodeURIComponent(href);
+'use strict';
 
-    document.location.href = 'http://read.text:85/?link=' + link;
+(function(){
+    function getFirstByAttribute(elms, attr, value) {
+        for (var i=0; i<elms.length; i++) {
+            var elm = elms[i];
+            if (elm.getAttribute(attr) == value) {
+                return elm;
+            }
+        }
+        return null;
+    }
 
+    var  head = document.head || document.getElementsByTagName('head')[0],
+         links = head.getElementsByTagName('link'),
+         printLink = getFirstByAttribute(links, 'media', 'print'),
+         cssParm = '',
+         urlParm,
+         redirect;
+
+    //console.log(printlink.href);
+        urlParm = '?link=' + encodeURIComponent(document.location.href);
+        if (printLink) {
+            cssParm = '&pcss=' + encodeURIComponent(printLink.href);
+        }
+        redirect = 'http://read.text:85/' + urlParm + cssParm;
+        //console.log(redirect);
+        document.location.href = redirect;
+
+})();
 
 
